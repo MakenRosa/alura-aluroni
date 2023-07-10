0a9ec6d2772aa5091bdbe8b8724283eac2e351b5
MakenRosa <maken.cristhian@gmail.com>
2023-07-10 14:02:05 -0300

    feat: implemented Filters funcionality

--- src/pages/Menu/Filters/index.tsx
+++ src/pages/Menu/Filters/index.tsx
@@ -1,18 +1,32 @@
+import { Dispatch, SetStateAction } from 'react'
 import filters from './filters.json'
 import styles from './Filters.module.scss'
+import classNames from 'classnames'
 
 type IOption = typeof filters[0]
 
-export default function Filters() {
+interface FiltersProps {
+    filter: number | null,
+    setFilter: Dispatch<SetStateAction<number | null>>
+}
+
+export default function Filters({ filter, setFilter }: FiltersProps) {
 
     const selectFilter = (option: IOption) => {
-        
+        if (filter === option.id) {
+            setFilter(null)
+        } else {
+            setFilter(option.id)
+        }
     }
 
     return (
         <div className={styles.filters}>
             {filters.map((option) => (
-                <button key={option.id} onClick={() => selectFilter(option)} className={styles.filters__button}>
+                <button className={classNames({
+                    [styles.filters__button]: true,
+                    [styles['filters__button--active']]: filter === option.id
+                    })} key={option.id} onClick={() => selectFilter(option)}>
                     {option.label}
                 </button>
             ))}
