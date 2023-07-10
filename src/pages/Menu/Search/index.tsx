8187fb83a073d994ae82353d347d78c29de2fe63
MakenRosa <maken.cristhian@gmail.com>
2023-07-10 14:04:01 -0300

    refactor: remove unnecessary import

--- src/pages/Menu/Search/index.tsx
+++ src/pages/Menu/Search/index.tsx
@@ -1,5 +1,4 @@
-import React, { Dispatch, SetStateAction } from "react";
-
+import { Dispatch, SetStateAction } from "react";
 import styles from './Search.module.scss'
 import { CgSearch } from 'react-icons/cg'
 
