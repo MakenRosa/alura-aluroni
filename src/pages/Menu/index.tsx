08fcab1f4d0a6893a9fadcb8bdc362b238dfe808
MakenRosa <maken.cristhian@gmail.com>
2023-07-12 12:34:51 -0300

    refactor: separated Menu structure

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,5 +1,5 @@
 import styles from './Menu.module.scss';
-import {ReactComponent as Logo} from 'assets/logo.svg';
+import stylesTheme from 'styles/Theme.module.scss';
 import Search from './Search';
 import { useState } from 'react';
 import Filters from './Filters';
@@ -12,24 +12,14 @@ export default function Menu() {
     const [filter, setFilter] = useState<number | null>(null);
     const [orderer, setOrderer] = useState('');
     return(
-        <main>
-            <nav className={styles.navbar}>
-                <Logo />
-            </nav>
-            <header className={styles.header}>
-                <div className={styles.header__text}>
-                    The House of Code and Dough.
-                </div>
-            </header>
-            <section className={styles.menu}>
-                <h3 className={styles.menu__title}>Menu</h3>
-                <Search search={search} setSearch={setSearch}/>
-                <div className={styles.menu__filters}>
-                    <Filters filter={filter} setFilter={setFilter}/>
-                    <Orderer orderer={orderer} setOrderer={setOrderer}/>
-                </div>
-                <Items search={search} filter={filter} orderer={orderer}/>
-            </section>
-        </main>
+        <section className={styles.menu}>
+            <h3 className={stylesTheme.title}>Menu</h3>
+            <Search search={search} setSearch={setSearch}/>
+            <div className={styles.menu__filters}>
+                <Filters filter={filter} setFilter={setFilter}/>
+                <Orderer orderer={orderer} setOrderer={setOrderer}/>
+            </div>
+            <Items search={search} filter={filter} orderer={orderer}/>
+        </section>
     );
 }
