a79ae12fe932ef08dfa4e071dbcc41a35bedcade
MakenRosa <maken.cristhian@gmail.com>
2023-07-13 13:24:22 -0300

    feat: added not found page  and dish path

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,19 +4,25 @@ import NavMenu from 'components/NavMenu';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import BasePage from 'pages/BasePage';
 import About from 'pages/About';
+import Footer from 'components/Footer';
+import NotFound from 'pages/NotFound';
+import Dish from 'pages/Dish';
 
 export default function AppRouter() {
     return (
         <Router>
-            <NavMenu />
-            <main>
+            <main className="container">
+                <NavMenu />
                 <Routes>
                     <Route path="/" element={<BasePage />}>
                         <Route index element={<Home />} />
                         <Route path="menu" element={<Menu />} />
                         <Route path="about" element={<About />} />
+                        <Route path="dish/:id" element={<Dish />} />
                     </Route>
+                    <Route path="*" element={<NotFound />} />
                 </Routes>
+                <Footer />
             </main>
         </Router>
     );
